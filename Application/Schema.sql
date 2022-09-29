b5c7dc5822d9220ecd5c706e4a74ad4662a7ebfa
MosesSupposes <mosesasamuel633@gmail.com>
2022-09-29 14:34:36 -0500

    add a db table for clinicians

--- Application/Schema.sql
+++ Application/Schema.sql
@@ -1,1 +1,8 @@
 -- Your database schema. Use the Schema Designer at http://localhost:8001/ to add some tables.
+CREATE TABLE clinicians (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+    full_name TEXT NOT NULL,
+    email TEXT DEFAULT NULL UNIQUE,
+    phone_number TEXT DEFAULT NULL,
+    profile_portrait TEXT DEFAULT NULL UNIQUE
+);
