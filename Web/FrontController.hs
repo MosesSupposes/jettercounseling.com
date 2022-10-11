ecc8633dac36b8fed4080607e4f65d3c3da41761
MosesSupposes <mosesasamuel633@gmail.com>
2022-10-11 01:32:13 -0500

    remove all references to old clinicians table

--- Web/FrontController.hs
+++ Web/FrontController.hs
@@ -5,14 +5,12 @@ import Web.Controller.Prelude
 import Web.View.Layout (defaultLayout)
 
 -- Controller Imports
-import Web.Controller.Clinicians
 import Web.Controller.Static
 
 instance FrontController WebApplication where
     controllers = 
         [ startPage WelcomeAction
         -- Generator Marker
-        , parseRoute @CliniciansController
         ]
 
 instance InitControllerContext WebApplication where
--- Web/Types.hs
+++ Web/Types.hs
@@ -9,12 +9,3 @@ data WebApplication = WebApplication deriving (Eq, Show)
 
 data StaticController = WelcomeAction deriving (Eq, Show, Data)
 
-data CliniciansController
-    = CliniciansAction
-    | NewClinicianAction
-    | ShowClinicianAction { clinicianId :: !(Id Clinician) }
-    | CreateClinicianAction
-    | EditClinicianAction { clinicianId :: !(Id Clinician) }
-    | UpdateClinicianAction { clinicianId :: !(Id Clinician) }
-    | DeleteClinicianAction { clinicianId :: !(Id Clinician) }
-    deriving (Eq, Show, Data)
